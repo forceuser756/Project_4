92aed4ecac29c16e605a16784501711cec17426e
TFossum1 <TFossum1@gmail.com>
2019-12-02 16:05:50 -0500

    css test

---
+++ 9_project4/gamedatabase.php
@@ -0,0 +1,49 @@
+<head>
+  <title>Nintendo 64 Database</title>
+</head>
+
+<body>
+  <h1>Nintendo 64 Database</h1>
+  <p>Listed below are all games from the Nintendo 64
+  </p>
+  <p>
+  That's pretty 90s, I thought, so I decided to make a website which celebrates the best of 1990s website design.
+  </p>
+
+  <table>
+    <tr>
+      <th>Feature</th>
+      <th>Method</th>
+      <th>Result</th>
+    </tr>
+    <tr>
+      <td>Awesome starfield background</td>
+      <td>Link to an image I uploaded to tinypic using the <b class="code">background-image:</b> CSS property</td>
+      <td><img src="https://res.cloudinary.com/teepublic/image/private/s--ecodLK39--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1565157970/production/designs/5535408_0.jpg" width=200></td>
+    </tr>
+    <tr>
+      <td>Overuse of animated gifs</td>
+      <td>Link to an image on tinypic using html img tab</td>
+      <td><img src="https://res.cloudinary.com/teepublic/image/private/s--ecodLK39--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1565157970/production/designs/5535408_0.jpg" width=200></td>
+    </tr>
+    <tr>
+      <td>Non-standard hyperlink colour</td>
+      <td>Set the colour of the a element using the <b class="code">color: </b>CSS property</td>
+      <td><img src="https://res.cloudinary.com/teepublic/image/private/s--ecodLK39--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1565157970/production/designs/5535408_0.jpg" width=200</td>
+    </tr>
+    <tr>
+      <td>Syntax Highlighting for CSS</td>
+      <td>Using the "Code" CSS class to highlight code elements with a different colour.</td>
+      <td><img src="http://i50.tinypic.com/nd3atd.jpg" width=200></td>
+    </tr>
+  </table>
+
+  <h2>Still to do</h2>
+
+  <ul>
+    <li>Keeping the background still while the text scrolls</li>
+    <ul>
+      <li>A working hit counter that looks like this: <img src="http://i44.tinypic.com/x6bthc.jpg"></li>
+      <li>or this: <img src="http://i49.tinypic.com/2udw42c.jpg"</li>
+    </ul>
+  </ul>
